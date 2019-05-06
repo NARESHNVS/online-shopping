bd4e37d6bd2ef46ca4f972f316a3a43aa0403790
Naresh NVS <nareshnvsrinivasan@gmail.com>
2019-05-06 15:54:32 +0530

    My Project Setup Completed Successfully

---
+++ ShoppingBackEnd/src/main/java/net/naresh/ShoppingBackEnd/App.java
@@ -0,0 +1,13 @@
+package net.naresh.ShoppingBackEnd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
